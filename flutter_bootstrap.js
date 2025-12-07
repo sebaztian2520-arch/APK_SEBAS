8d0acc55e16d18237f3748e4d85c24b389918d5c
sebaztian2520-arch <sebaztian2520@gmail.com>
2025-12-07 00:42:24 -0500

    Add files via upload
    build web con app final

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2485596801"
+    serviceWorkerVersion: "1526602437"
   }
 });
